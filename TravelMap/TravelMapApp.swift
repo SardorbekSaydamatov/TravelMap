eea13435214526477a8f86468c96227f8595992f
SardorbekSaydamatov <saidalo.saydamatov@gmail.com>
2024-11-06 11:09:15 +0500

    Initial Commit

---
+++ TravelMap/TravelMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelMapApp.swift
+//  TravelMap
+//
+//  Created by Sardorbek Saydamatov on 06/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TravelMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
